35fa668a06e69a4ed99b79b4718752877f5eff22
nearworld <artemismars2@outlook.com>
2025-03-05 10:52:10 +0900

    feat: kakao login api

--- app/api/auth/kakao/route.ts
+++ app/api/auth/kakao/route.ts
@@ -2,22 +2,20 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
   const { code } = await req.json();
+  console.log(code);
+  console.log(process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID);
 
   try {
-    const tokenResponse = await fetch("https://kauth.kakao.com/oauth/token", {
+    const tokenResponse = await fetch(`https://kauth.kakao.com/oauth/token`, {
       method: "POST",
       headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
+        "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
       },
-      body: new URLSearchParams({
-        grant_type: "authorization_code",
-        client_id: process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID!,
-        redirect_uri: process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI!,
-        code,
-      }),
+      body: `grant_type=authorization_code&client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID!}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI!}&code=${code}&client_secret=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_SECRET!}`,
     });
+    const res = await tokenResponse.json();
 
-    const { access_token } = await tokenResponse.json();
+    const { access_token } = res;
 
     const userResponse = await fetch("https://kapi.kakao.com/v2/user/me", {
       headers: {
