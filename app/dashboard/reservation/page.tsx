4b18c5aad7399b567d09faa6a20995ccdaae1b47
nearworld <artemismars2@outlook.com>
2025-03-05 18:03:23 +0900

    feat: dashboard page

---
+++ app/dashboard/reservation/page.tsx
@@ -0,0 +1,15 @@
+// app/dashboard/reservation/page.tsx
+
+export default function ReservationPage() {
+  return (
+    <div className="flex min-h-screen">
+      <div className="flex flex-1 flex-col">
+        <main className="flex-1 p-6">
+          <div className="container mx-auto flex h-full items-center justify-center">
+            <p className="text-muted-foreground">예약 페이지</p>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
---
+++ components/nav-main.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import {
+  Box,
+  ChevronRight,
+  Monitor,
+  NotebookText,
+  type LucideIcon,
+} from "lucide-react";
+
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import {
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+} from "@/components/ui/sidebar";
+import { useRouter } from "next/navigation";
+
+export function NavMain({
+  items,
+}: {
+  items: {
+    title: string;
+    url: string;
+    icon?: LucideIcon;
+    isActive?: boolean;
+    items?: {
+      title: string;
+      url: string;
+    }[];
+  }[];
+}) {
+  const router = useRouter();
+
+  return (
+    <SidebarGroup>
+      <SidebarGroupLabel>업무 관리</SidebarGroupLabel>
+      <SidebarMenu>
+        <SidebarMenuItem>
+          <SidebarMenuButton
+            tooltip={"종합"}
+            className="cursor-pointer"
+            onClick={() => router.replace("/dashboard")}
+          >
+            <Monitor />
+            <span>종합</span>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+        <SidebarMenuItem>
+          <SidebarMenuButton
+            tooltip={"식당 예약관리"}
+            className="cursor-pointer"
+            onClick={() => router.replace("/dashboard/reservation")}
+          >
+            <NotebookText />
+            <span>예약 관리</span>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+        <SidebarMenuItem>
+          <SidebarMenuButton
+            tooltip={"식당 재고관리"}
+            className="cursor-pointer"
+            onClick={() => router.replace("/dashboard/inventory")}
+          >
+            <Box />
+            <span>재고 관리</span>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+        {items.map((item) => (
+          <Collapsible
+            key={item.title}
+            asChild
+            defaultOpen={item.isActive}
+            className="group/collapsible"
+          >
+            <SidebarMenuItem>
+              <CollapsibleTrigger asChild>
+                <SidebarMenuButton tooltip={item.title}>
+                  {item.icon && <item.icon />}
+                  <span>{item.title}</span>
+                  <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+                </SidebarMenuButton>
+              </CollapsibleTrigger>
+              <CollapsibleContent>
+                <SidebarMenuSub>
+                  {item.items?.map((subItem) => (
+                    <SidebarMenuSubItem key={subItem.title}>
+                      <SidebarMenuSubButton asChild>
+                        <a href={subItem.url}>
+                          <span>{subItem.title}</span>
+                        </a>
+                      </SidebarMenuSubButton>
+                    </SidebarMenuSubItem>
+                  ))}
+                </SidebarMenuSub>
+              </CollapsibleContent>
+            </SidebarMenuItem>
+          </Collapsible>
+        ))}
+      </SidebarMenu>
+    </SidebarGroup>
+  );
+}
