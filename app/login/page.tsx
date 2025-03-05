0a6254306a8a91ce7983e606c7867d943fa5f86d
nearworld <artemismars2@outlook.com>
2025-03-05 10:01:29 +0900

    chore: Formatting code

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,6 @@
 import Image from "next/image";
 
 export default function LoginPage() {
-
   // 카카오 로그인 URL 생성
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}`;
 
@@ -13,13 +12,11 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
       {/* 로고 */}
-      <h1 className="text-3xl md:text-4xl font-bold text-gray-800 mb-8 text-center">
-        <p>사회적협동조합
-            </p><p className="mt-2">
-            청소년자립학교
-                </p>
+      <h1 className="mb-8 text-center text-3xl font-bold text-gray-800 md:text-4xl">
+        <p>사회적협동조합</p>
+        <p className="mt-2">청소년자립학교</p>
       </h1>
 
       {/* 카카오 로그인 버튼 */}
@@ -29,7 +26,7 @@ export default function LoginPage() {
           alt="카카오 로그인 버튼"
           width={200}
           height={45}
-          className="hover:opacity-80 transition-opacity"
+          className="transition-opacity hover:opacity-80"
         />
       </button>
     </div>
