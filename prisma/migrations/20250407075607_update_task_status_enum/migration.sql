20cd356f286b7604944ab27260773a084a061b5c
nearworld <artemismars2@outlook.com>
2025-04-07 16:57:54 +0900

    fix: Prisma Task model

---
+++ prisma/migrations/20250407075607_update_task_status_enum/migration.sql
@@ -0,0 +1,19 @@
+-- CreateEnum
+CREATE TYPE "TaskStatus" AS ENUM ('INCOMPLETE', 'COMPLETE');
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "status" "TaskStatus" NOT NULL DEFAULT 'INCOMPLETE',
+    "dueDate" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "assigneeId" TEXT,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Task" ADD CONSTRAINT "Task_assigneeId_fkey" FOREIGN KEY ("assigneeId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
