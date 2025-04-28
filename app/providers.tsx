793b1417c33eea0a5e852d2e737dfea2384fc923
Jun <wnwjdwns1@naver.com>
2025-04-28 12:36:42 +0900

    ✨ feat(auth): AuthCheckProvider를 추가하여 사용자 인증 상태 확인 기능 추가

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,5 +1,12 @@
+"use client";
+
 import { SessionProvider } from "next-auth/react";
+import { AuthCheckProvider } from "./auth-check-provider";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <AuthCheckProvider>{children}</AuthCheckProvider>
+    </SessionProvider>
+  );
 }
