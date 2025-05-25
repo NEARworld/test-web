1f8d8d867a8e604f8bb8b307f0a4bca8167e82e2
nearworld <artemismars2@outlook.com>
2025-05-25 21:10:25 +0900

    ✨ feat(community): community 및 other 페이지 추가

---
+++ app/dashboard/documents/community/page.tsx
@@ -0,0 +1,4 @@
+// 한글 주석: community 페이지
+export default function CommunityPage() {
+  return <div>Community Page</div>;
+}
---
+++ app/dashboard/documents/other/page.tsx
@@ -0,0 +1,4 @@
+// 한글 주석: other 페이지
+export default function OtherPage() {
+  return <div>Other Page</div>;
+} 
