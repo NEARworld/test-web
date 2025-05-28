d45e5d7ecae8327c5c3dbc343ee40ee552c62419
nearworld <artemismars2@outlook.com>
2025-05-28 14:21:25 +0900

    ✨ refactor(DocumentsLayout): DocumentProvider에서 boardType 파라미터 제거

--- app/dashboard/documents/layout.tsx
+++ app/dashboard/documents/layout.tsx
@@ -1,17 +1,13 @@
 import { DocumentProvider } from "@/contexts/DocumentProvider";
-import { usePathname } from "next/navigation";
 
 export default function DocumentsLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const pathname = usePathname();
-  const boardType = pathname.split("/").pop();
-
   return (
     <section className="mx-4">
-      <DocumentProvider boardType={boardType}>{children}</DocumentProvider>
+      <DocumentProvider>{children}</DocumentProvider>
     </section>
   );
 }
