0caa911e336de725a06862bee8ea6900536f432b
nearworld <artemismars2@outlook.com>
2025-05-31 17:55:10 +0900

    ✨ feat(EnumConverters): 사용자 직무 및 부서에 대한 한국어 변환 함수 및 타입 추가

--- lib/enum-converters.ts
+++ lib/enum-converters.ts
@@ -1,7 +1,54 @@
-import { UserStatus } from "@prisma/client";
+import { Department, JobPosition, UserStatus } from "@prisma/client";
 
 export type UserStatusKorean = "재직중" | "비활성" | "휴직" | "퇴사";
 
+export type UserJobPositionKorean =
+  | "직원"
+  | "팀장"
+  | "사무국장"
+  | "대표"
+  | "이사장"
+  | "미정";
+
+export type UserDepartmentKorean =
+  | "바자울"
+  | "청년식당"
+  | "먹거리 돌봄 센터"
+  | "미정";
+
+export function convertUserDepartmentToKorean(
+  department: Department,
+): UserDepartmentKorean {
+  switch (department) {
+    case Department.BAZAUL:
+      return "바자울";
+    case Department.YOUTH_RESTAURANT:
+      return "청년식당";
+    case Department.FOOD_CARE_CENTER:
+      return "먹거리 돌봄 센터";
+    default:
+      return "미정";
+  }
+}
+
+export function convertUserJobPositionToKorean(
+  position: JobPosition,
+): UserJobPositionKorean {
+  switch (position) {
+    case JobPosition.STAFF:
+      return "직원";
+    case JobPosition.TEAM_LEADER:
+      return "팀장";
+    case JobPosition.GENERAL_SECRETARY:
+      return "사무국장";
+    case JobPosition.CHAIRPERSON:
+      return "대표";
+    case JobPosition.CEO:
+      return "이사장";
+    default:
+      return "미정";
+  }
+}
 export function convertUserStatusToKorean(
   status: UserStatus,
 ): UserStatusKorean {
