c5bf71bc973ae19abc5cd6e60630a2a17e65041c
nearworld <artemismars2@outlook.com>
2025-06-02 12:40:34 +0900

    ✨ feat(ApprovalsPage): 탭 컴포넌트를 추가하여 결재 상태 필터링 기능 구현

--- app/dashboard/approvals/page.tsx
+++ app/dashboard/approvals/page.tsx
@@ -1,15 +1,39 @@
 // app/approvals/page.tsx
 "use client"; // shadcn/ui 컴포넌트는 클라이언트 컴포넌트여야 합니다.
 
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import React from "react";
 
 const ApprovalsPage: React.FC = () => {
+  const [, setActiveTab] = React.useState<"All" | "Pending" | "Completed">(
+    "All",
+  );
+
   return (
     <div className="font-inter min-h-screen md:px-4">
       <div className="max-w-7xl px-4">
         <header className="mb-8">
           <h1 className="text-3xl font-bold text-gray-900">전자 결재 시스템</h1>
         </header>
+
+        <Tabs
+          defaultValue="All"
+          onValueChange={(value) =>
+            setActiveTab(value as "All" | "Pending" | "Completed")
+          }
+        >
+          <TabsList className="mb-6 grid w-full grid-cols-3 md:w-[400px]">
+            <TabsTrigger value="All" className="cursor-pointer">
+              모든 결재
+            </TabsTrigger>
+            <TabsTrigger value="Pending" className="cursor-pointer">
+              결재 대기
+            </TabsTrigger>
+            <TabsTrigger value="Completed" className="cursor-pointer">
+              결재 완료
+            </TabsTrigger>
+          </TabsList>
+        </Tabs>
       </div>
     </div>
   );
